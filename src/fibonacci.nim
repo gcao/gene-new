579803dd4f11ffa65e30df0d54dd7f61712f63f8
Guoliang Cao <gcao99@gmail.com>
2021-02-21 11:18:52 -0500

    Benchmark fibonacci(24)

---
+++ src/fibonacci.nim
@@ -0,0 +1,26 @@
+when isMainModule:
+  import times
+
+  import ./gene/types
+  import ./gene/translators
+  import ./gene/interpreter
+
+  init_app_and_vm()
+
+  var code = """
+    (fn fib n
+      (if (n < 2)
+        n
+      else
+        ((fib (n - 1)) + (fib (n - 2)))
+      )
+    )
+    (fib 24)
+  """
+  let e = translate(VM.prepare(code))
+  let module = new_module()
+  var frame = Frame(ns: module.root_ns, scope: new_scope(), self: Nil)
+  let start = cpuTime()
+  let result = VM.eval(frame, e)
+  echo "Time: " & $(cpuTime() - start)
+  echo "fib(24) = " & $result
