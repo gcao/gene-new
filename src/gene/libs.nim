aa4cfe5f02b3af7e40185bec98ce9d0f892f9499
Guoliang Cao <gcao99@gmail.com>
2021-10-09 11:31:49 -0400

    Standard library

--- src/gene/libs.nim
+++ src/gene/libs.nim
@@ -1,5 +1,6 @@
 import ./types
 
 proc init*() =
-  ObjectClass = Value(kind: VkClass, class: new_class("Object"))
-  GENE_NS.ns["Object"] = ObjectClass
+  VmCreatedCallbacks.add proc(self: VirtualMachine) =
+    ObjectClass = Value(kind: VkClass, class: new_class("Object"))
+    GENE_NS.ns["Object"] = ObjectClass
