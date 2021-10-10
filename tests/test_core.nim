2c476c53c41ca07aebb2ba9a6b13f53d561cc4d9
Guoliang Cao <gcao99@gmail.com>
2021-10-10 11:23:55 -0400

    Minor changes

--- tests/test_core.nim
+++ tests/test_core.nim
@@ -1,21 +1,29 @@
-import unittest
+# import unittest
 
-import gene/types
+# import gene/types
 
-import ./helpers
+# import ./helpers
 
-test_core "gene", proc(r: GeneValue) =
-  check r.internal.ns.name == "gene"
+# test "GeneAny":
+#   var s = "abc"
+#   var g = GeneValue(
+#     kind: GeneAny,
+#     any: cast[pointer](s.addr),
+#   )
+#   check cast[ptr string](g.any)[] == s
 
-test_core "genex", proc(r: GeneValue) =
-  check r.internal.ns.name == "genex"
+# test_core "gene", proc(r: GeneValue) =
+#   check r.internal.ns.name == "gene"
 
-test_core "(assert true)", GeneNil
+# test_core "genex", proc(r: GeneValue) =
+#   check r.internal.ns.name == "genex"
 
-test_core "(AssertionError .name)", "AssertionError"
+# test_core "(assert true)", GeneNil
 
-# test_core """
-#   $runtime
-# """, proc(r: GeneValue) =
-#   check r.internal.runtime.home == "/Users/gcao/proj/gene.nim"
-#   check r.internal.runtime.version == "0.1.0"
+# test_core "(AssertionError .name)", "AssertionError"
+
+# # test_core """
+# #   $runtime
+# # """, proc(r: GeneValue) =
+# #   check r.internal.runtime.home == "/Users/gcao/proj/gene.nim"
+# #   check r.internal.runtime.version == "0.1.0"
