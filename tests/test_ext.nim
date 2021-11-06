b3c75fd78dff52780b9ad8fd0902df291470054d
Guoliang Cao <gcao99@gmail.com>
2021-11-06 15:05:33 -0400

    Native extension

--- tests/test_ext.nim
+++ tests/test_ext.nim
@@ -1,6 +1,7 @@
 import unittest, tables
 
 import gene/types
+import gene/interpreter
 
 import ./helpers
 
@@ -9,7 +10,13 @@ import ./helpers
 #   var data = @[1, 2]
 #   check r(props, data) == 3
 
-test_interpreter """
-  (import_native from "tests/libextension")
+proc test_extension*(code: string, result: Value) =
+  var code = cleanup(code)
+  test "Interpreter / eval: " & code:
+    init_all()
+    discard VM.eval("(import_native from \"tests/libextension\")")
+    check VM.eval(code) == result
+
+test_extension """
   (test 1)
 """, 1
