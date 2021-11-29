e098b92e6090c5eb424e6b4cd4ae581b347631bb
Guoliang Cao <gcao99@gmail.com>
2021-11-29 15:25:28 -0500

    Native SQLite extension

--- src/genex/sqlite.nim
+++ src/genex/sqlite.nim
@@ -1,10 +1,81 @@
 include gene/extension/boilerplate
-import gene/utils
+
+type
+  Clause = ref object of RootObj
+
+  TableLike = ref object of Clause
+    alias: string
+
+  Table = ref object of TableLike
+    name: string
+
+  # PseudoTable = ref object of TableLike
+  #   stmt: Select
+
+  JoinKind = enum
+    JkDefault
+    # JkLeft
+    # JkRight
+    # JkInner
+    # JkOuter
+
+  Join = ref object of Clause
+    table: TableLike
+    kind: JoinKind
+    conditions: seq[Condition]
+
+  ColumnLike = ref object of Clause
+    alias: string
+
+  Column = ref object of ColumnLike
+    table_name: string
+    name: string
+
+  # PseudoColumn = ref object of ColumnLike
+  #   clause: Clause # not all clause can work like a column
+
+  Literal = ref object of ColumnLike
+    value: Value
+
+  Condition = ref object of Clause
+
+  BinOp = enum
+    BinAnd
+
+  BinClause = ref object of ColumnLike
+    op: BinOp
+    left, right: Clause
+
+  # Call = ref object of Clause
+  #   name: string
+  #   args: seq[Clause]
+
+  Statement = ref object of Clause
+
+  Select = ref object of Statement
+    tables: seq[TableLike]
+    joins: seq[Join]
+    cols: seq[ColumnLike]
+    conditions: seq[Condition]
+
+  ExSelect = ref object of Expr
+
+proc eval_select(self: VirtualMachine, frame: Frame, target: Value, expr: var Expr): Value =
+  var expr = cast[ExSelect](expr)
+  # self.eval(frame, expr.data)
+  todo()
+
+proc translate_select(value: Value): Expr {.wrap_exception.} =
+  return ExSelect(
+    evaluator: eval_wrap(eval_select),
+  )
 
 {.push dynlib exportc.}
 
 proc init*(): Value {.wrap_exception.} =
   result = new_namespace("sqlite")
   GENEX_NS.ns["sqlite"] = result
+
+  result.ns["select"] = new_gene_processor(translate_select)
 
 {.pop.}
--- tests/test_stdlib_sqlite.nim
+++ tests/test_stdlib_sqlite.nim
@@ -11,14 +11,27 @@ import ./helpers
 # Drop table
 # Open/close db connection
 # Insert
+#   E.g. INSERT INTO t(...)
+#        VALUES
+#          (...),
+#          (...)
 # Select
 #   E.g. SELECT a.*, TRIM(b.name) AS name
 #        FROM table_a a
 #        JOIN table_b b ON a.id=b.id
 #        WHERE a.id IN (...)
+#        GROUP BY a.type
+#        HAVING SUM(a.amount) > 100
 #        ORDER BY b DESC
 # Update
+#   E.g. UPDATE t
+#        SET name=UPPER(name)
+#        WHERE ...
 # Delete
+#   E.g. DELETE FROM t
+#        WHERE ...
+
+var db_file = "/tmp/gene-test.db"
 
 proc test_sql(code: string, callback: proc(result: Value)) =
   var code = cleanup(code)
@@ -28,11 +41,30 @@ proc test_sql(code: string, callback: proc(result: Value)) =
 suite "SQLite":
   init_all()
   discard VM.eval("""
-    # Recreate sqlite db
     (import from "build/libsqlite" ^^native)
   """)
 
+  setup:
+    # Recreate sqlite db
+    exec "rm " & db_file
+    let db = open(db_file, "", "", "")
+    db.exec(sql"""
+      DROP TABLE IF EXISTS table_a
+    """)
+    db.exec(sql"""
+      CREATE TABLE table_a (
+        id   INTEGER,
+        name VARCHAR(50) NOT NULL
+      )
+    """)
+    db.exec(sql"""
+      INSERT INTO my_table (id, name) VALUES (1, 'John')
+      INSERT INTO my_table (id, name) VALUES (2, 'Mark')
+    """)
+    db.close()
+
   test_sql """
-    (genex/select * from test)
+    (var db (genex/sqlite/open "/tmp/gene-test.db"))
+    (db .exec (genex/sqlite/select * from table_a))
   """, proc(r: Value) =
-    todo()
+    check r.vec.len == 2
