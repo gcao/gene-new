2b90e24409e94f0b63dcb50a978e433394c2e572
Guoliang Cao <gcao99@gmail.com>
2023-11-06 16:32:36 -0500

    Issue 81: data type changes

--- src/gene/value.nim
+++ src/gene/value.nim
@@ -2,139 +2,65 @@ import re, bitops, strformat
 
 type
   ValueKind* = enum
-    # VkVoid vs VkNil vs VkPlaceholder:
-    #   VkVoid has special meaning in some places (e.g. templates)
-    #   Value(kind: VkNil) and nil should have same meaning and is the default/uninitialized value.
-    #   VkPlaceholder can be interpreted any way we want
+    # void vs nil vs placeholder:
+    #   void has special meaning in some places (e.g. templates)
+    #   nil is the default/uninitialized value.
+    #   placeholder can be interpreted any way we want
     VkVoid
     VkNil
     VkPlaceholder
-    # VkAny vs VkCustom:
-    # VkAny can be used to represent anything
-    # VkCustom can be used to represent any value that inherits from CustomValue
-    VkAny
     VkPointer
-    VkCustom
     VkBool
     VkInt
-    VkRatio
     VkFloat
-    VkBin
-    VkBin64
-    VkByte
-    VkBytes
-    # VkPercent # xx.xx%
     VkChar
-    VkString
-    VkSymbol
-    VkComplexSymbol
-    VkRegex
-    VkRegexMatch
-    VkRange
-    VkSelector
-    VkQuote
-    VkUnquote
-    VkReference
-    VkRefTarget
-    # Time part should be 00:00:00 and timezone should not matter
-    VkDate
-    # Date + time + timezone
-    VkDateTime
-    VkTime
-    VkTimezone
-    VkVector
-    VkMap
-    VkSet
-    VkGene
-    VkArguments
-    VkStream
-    VkDocument
-    VkFile
-    VkArchiveFile
-    VkDirectory
-    # Internal types
-    VkException = 128
-    VkGeneProcessor
-    VkApplication
-    VkPackage
-    VkModule
-    VkNamespace
-    VkFunction
-    VkBoundFunction
-    VkMacro
-    VkBlock
-    VkClass
-    VkMixin
-    VkMethod
-    VkBoundMethod
-    VkNativeFn
-    VkNativeFn2
-    VkNativeMethod
-    VkNativeMethod2
-    VkInstance
-    VkCast
-    VkEnum
-    VkEnumMember
-    # VkInterception
-    # VkExpr
-    VkExplode
-    VkFuture
-    VkThread
-    VkThreadMessage
-    VkJson
-    VkNativeFile
-    VkCuId
-    VkCompilationUnit
+    VkRune  # Unicode code point = u32
 
   Value* = distinct int64
+
+const I64_MASK = 0xC000_0000_0000_0000u64
+const F64_ZERO = 0x2000_0000_0000_0000u64
+
+const NIL_PREFIX = 0x7FFA
+const NIL* = cast[Value](0x7FFA_A000_0000_0000u64)
+
+const BOOL_PREFIX = 0x7FFC
+const TRUE*  = cast[Value](0x7FFC_A000_0000_0000u64)
+const FALSE* = cast[Value](0x7FFC_0000_0000_0000u64)
+
+const POINTER_PREFIX = 0x7FFB
+
+const OTHER_PREFIX = 0x7FFE
+const OTHER_MASK = 0x7FFE_FF00_0000_0000u64
+
+const VOID* = cast[Value](0x7FFE_0000_0000_0000u64)
+const PLACEHOLDER* = cast[Value](0x7FFE_0100_0000_0000u64)
+
+const CHAR_PREFIX = 0xFFFE
+const CHAR_MASK = 0xFFFE_0000_0000_0000u64
 
 proc todo*() =
   raise new_exception(Exception, "TODO")
 
-# const NAN_MASK = 0x7FFA000000000000
-const I64_MASK = 0xC000000000000000
-const F64_ZERO = 0x2000000000000000
-
-const NIL_MASK = 0x7FFA
-const NIL* = cast[Value](0x7FFAA00000000000)
-
-const BOOL_MASK = 0x7FFC
-const TRUE* = cast[Value](0x7FFCA00000000000)
-const FALSE* = cast[Value](0x7FFC000000000000)
-
-const POINTER_MASK = 0x7FFB
-
-const OTHER_MASK = 0x7FFE
-
-const VOID* = cast[Value](0x7FFE000000000000)
-const PLACEHOLDER* = cast[Value](0x7FFE010000000000)
-
-# proc is_i64*(v: Value): bool {.inline.} =
-#   bitor(cast[int64](v).shl(1), 0x3FFFFFFFFFFFFFFF) == 0x3FFFFFFFFFFFFFFF
-
-# proc is_f64*(v: Value): bool {.inline.} =
-#   bitor(cast[int64](v).shl(1), 0x3FFFFFFFFFFFFFFF) != 0x3FFFFFFFFFFFFFFF
-
-# proc to_int*(v: Value): int64 {.inline.} = cast[int64](v)
-# proc to_value*(v: int64): Value {.inline.} = cast[Value](v)
-
-proc to_binstr*(v: int): string =
-  re.replacef(fmt"{v:064b}", re.re"([01]{8})", "$1 ")
+proc to_binstr*(v: int64): string =
+  re.replacef(fmt"{v: 065b}", re.re"([01]{8})", "$1 ")
 
 proc `==`*(a, b: Value): bool {.inline.} =
   cast[int64](a) == cast[int64](b)
 
 proc kind*(v: Value): ValueKind {.inline.} =
-  let v1 = cast[int64](v)
-  case v1.shr(48):
-    of NIL_MASK:
+  let v1 = cast[uint64](v)
+  case cast[int](v1.shr(48)):
+    of NIL_PREFIX:
       return VkNil
-    of BOOL_MASK:
+    of BOOL_PREFIX:
       return VkBool
-    of POINTER_MASK:
+    of POINTER_PREFIX:
       return VkPointer
-    of OTHER_MASK:
-      let other_info = cast[Value](bitand(v1, 0x7FFEFF0000000000))
+    of CHAR_PREFIX:
+      return VkChar
+    of OTHER_PREFIX:
+      let other_info = cast[Value](bitand(v1, OTHER_MASK))
       case other_info:
         of VOID:
           return VkVoid
@@ -148,8 +74,14 @@ proc kind*(v: Value): ValueKind {.inline.} =
       else:
         return VkFloat
 
+proc is_nil*(v: Value): bool {.inline.} =
+  v == NIL
+
+proc to_int*(v: Value): int64 {.inline.} =
+  cast[int64](v)
+
 proc to_float*(v: Value): float64 {.inline.} =
-  if cast[int64](v) == F64_ZERO:
+  if cast[uint64](v) == F64_ZERO:
     return 0.0
   else:
     return cast[float64](v)
@@ -161,16 +93,7 @@ proc to_value*(v: float64): Value {.inline.} =
     return cast[Value](v)
 
 proc to_bool*(v: Value): bool {.inline.} =
-  if v == TRUE:
-    return true
-  let v = cast[int64](v)
-  case v.shr(48):
-    of NIL_MASK:
-      return false
-    of BOOL_MASK:
-      return false
-    else:
-      return false
+  not (v == FALSE or v == NIL)
 
 proc to_value*(v: bool): Value {.inline.} =
   if v:
@@ -182,4 +105,13 @@ proc to_pointer*(v: Value): pointer {.inline.} =
   cast[pointer](bitand(cast[int64](v), 0x0000FFFFFFFFFFFF))
 
 proc to_value*(v: pointer): Value {.inline.} =
-  cast[Value](bitor(cast[int64](v), 0x7FFB000000000000))
+  if v.is_nil:
+    return NIL
+  else:
+    cast[Value](bitor(cast[int64](v), 0x7FFB000000000000))
+
+proc to_char*(v: Value): char {.inline.} =
+  todo()
+
+proc to_value*(v: char): Value {.inline.} =
+  cast[Value](bitor(CHAR_MASK, v.ord.uint64))
--- tests/test_value.nim
+++ tests/test_value.nim
@@ -4,18 +4,31 @@ import bitops
 import gene/value
 
 test "Value kind":
-  check 0.Value.kind == VkInt
   check NIL.kind == VkNil
   check VOID.kind == VkVoid
   check PLACEHOLDER.kind == VkPlaceholder
+
   check TRUE.kind == VkBool
   check FALSE.kind == VkBool
+
+  check 0.Value.kind == VkInt
+
   var a = 1
   check a.addr.to_value().kind == VkPointer
 
+  check 'a'.to_value().kind == VkChar
+
 test "Value conversion":
+  check nil.to_value().is_nil() == true
+  check nil.to_value() == NIL
+
+  check true.to_value().to_bool() == true
+  check false.to_value().to_bool() == false
+  check NIL.to_bool() == false
+  check 0.Value.to_bool() == true
+
+  check 1.Value.to_int() == 1
   check 0x20.shl(56).Value.to_float() == 0.0
   check 1.1.to_value().to_float() == 1.1
-  check 0.Value.to_bool() == false
   var a = 1
-  check cast[ptr int](a.addr.to_value().to_pointer())[] == 1
+  check cast[ptr int64](a.addr.to_value().to_pointer())[] == 1
